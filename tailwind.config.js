a9e24c6949aea8b0b654465dd87bb618fbb46e9e
Carlos Gerardo Perez Horta <carlos.perez@charliegperez.com>
2021-04-18 23:49:04 -0500

    chore(files): setting theme color and screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./dist/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      purple: colors.violet,
+      pink: colors.pink,
+      black: colors.black,
+      white: colors.white,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {},
   },
   variants: {
